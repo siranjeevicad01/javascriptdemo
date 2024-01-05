e1db37ac5b771c5c4edca1952267cf71ce20ad4c
siranjeevicad01@gmail.com <siranjeevicad01@brightskills.com>
2024-01-05 15:38:04 +0530

    wrk a console.log

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,18 @@
-alert("Just click the OK button")
+//alert("Just click the OK button")
+
+console.log(10);
+console.log({fname:"Jeevi", lname:"S"});
+
+
+var a = 30;
+let b = 50;
+const d = 70;
+c=a+b
+console.log(a);
+console.log(b);
+console.log("c :"+c);
+ 
+console.log({fname:"Jeevi", lname:"S"});
+console.table({fname:"Jeevi", lname:"S"});
+ 
+console.log(d);
