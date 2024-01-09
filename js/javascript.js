31d9aa64d7aea2f5bf3d12c19ce37146a750fb75
siranjeevicad01@gmail.com <siranjeevicad01@brightskills.com>
2024-01-09 18:00:23 +0530

    worked with a js

--- js/javascript.js
+++ js/javascript.js
@@ -43,3 +43,76 @@ var checkgrade1 = (a,b)=>{
     return a+b;
 }
 console.log(checkgrade1(1,2));
+
+
+
+
+function signals(){
+signal="red"
+if(signal=="red"){
+    console.log("Stop");
+}
+else if(signal=="green"){
+    console.log("go");
+}
+else if(signal=="orange"){
+    console.log("wait");
+}
+}
+signals()
+
+
+for(count=1;count>=10;count=count+1){
+    console.log(count);
+}
+
+
+function arithmetic(a,b){
+    console.log("Value of A is :",a, "and The value of B is :",b);   
+    console.log("add :",a+b);   
+    console.log("sub :",a-b);   
+    console.log("mul :",a*b);   
+    console.log("div :",a/b);   
+    console.log("mod :",a%b);   
+}
+arithmetic(2,3)
+
+
+
+
+function check(explore){
+    switch(explore){
+        case 1:
+            console.log("Statement 1 has executed");
+            break;
+        case 2:
+            console.log("Statement 2 successfully verified");
+            break;
+        case 3:
+            console.log("Not more much enough for statement 3");
+            break;
+        default:
+            console.log("There no case in this field");
+
+    }
+}
+check(4)
+
+function checkstring(a){
+    switch(a){
+        case 'py':
+            console.log("The value is A");
+            break;
+        case 'js':
+            console.log("The value is B");
+            break;
+        case 'sq':
+            console.log("The value is C");
+            break;
+        default:
+            console.log("This is not valied");
+    }
+}
+checkstring(prompt("Enter some other"))
+
+
